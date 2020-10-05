654826192a566a8db666f93b48bb40e13821a82e
Taewoo Lee <lteawoo@naver.com>
2020-10-05 21:51:34 +0900

    client

--- clients/client1/src/main/java/kr/taeu/client/web/controller/ClientController.java
+++ clients/client1/src/main/java/kr/taeu/client/web/controller/ClientController.java
@@ -2,11 +2,31 @@ package kr.taeu.client.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.UUID;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class ClientController {
     @GetMapping()
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/sso")
+    public String sso(HttpServletRequest httpServletRequest) {
+        String state = UUID.randomUUID().toString();
+        httpServletRequest.getSession().setAttribute("state", state);
+
+        StringBuilder builder = new StringBuilder();
+        builder.append("redirect:")
+                .append("http://localhost:8090/oauth/authorize")
+                .append("?response_type=code")
+                .append("&client_id=taeu_client")
+                .append("&redirect_uri=http://localhost:8091/oauth/callback")
+                .append("&state=")
+                .append(state);
+        return builder.toString();
     }
 }
--- sso-server/src/main/resources/data.sql
+++ sso-server/src/main/resources/data.sql
@@ -5,6 +5,6 @@ INSERT INTO OAUTH_CLIENT_DETAILS (CLIENT_ID, CLIENT_SECRET,
     AUTOAPPROVE, LOGOUT_URI, BASE_URI)
   VALUES ('taeu_client', 'taeu_secret',
     NULL, 'read', 'authorization_code,refresh_code',
-    'http://localhost:8090/oauth/callback', 'MY_CLIENT', 36000,
+    'http://localhost:8091/oauth/callback', 'MY_CLIENT', 36000,
     2592000, NULL,
     'true', 'http://localhost:8080/logout', 'http://localhost:8080/me');
